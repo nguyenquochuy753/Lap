5f2b8c3d147bfd05f000aff522c50651538555e2
nguyenquochuy753 <92136604+nguyenquochuy753@users.noreply.github.com>
2022-11-17 21:31:24 +0700

    Add files via upload

--- Lap 4/FE/src/exercise/exercise 1_3/Exercise3.js
+++ Lap 4/FE/src/exercise/exercise 1_3/Exercise3.js
@@ -1,76 +1,71 @@
-import React,{useState} from 'react'
 import "typeface-roboto";
+import React, { useState } from "react";
 import Drawer from "@mui/material/Drawer";
-import Button from '@mui/material/Button';
-import List from "@mui/material/List";
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import {BrowserRouter as Router , Route , Switch , Link} from "react-router-dom";
-import First from "./FirstPage";
-import Second from "./SecondPage";
-import Third from "./ThirdPage";
+import { Button, List, ListItem, ListItemText } from "@mui/material";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import First from './FirstPage';
+import Second from './SecondPage';
+import Third from './ThirdPage';
+function Exercise3({ links }) {
+  const [open, setOpen] = useState(false);
 
-
-
-export default function Exercise3({links}) {
-  const [open , setOpen] = useState(false);
-  function toggleDrawer({type , key}){
-    if(type === "Keydơwn" && (key === "Tab" || key === "shift")){
+  function toggleDrawer({ type, key }) {
+    if (type === "keydown" && (key === "Tab" || key === "Shift")) {
       return;
     }
+
     setOpen(!open);
   }
+  //thay component bằng element trong Route
   return (
-    <div>
-      <Router>
-        <Button onClick={toggleDrawer}>Open Nav</Button>
-        <section>
-          <Route path='/first' component={First}/>
-          <Route path='/second' component={Second}/>
-          <Route path='/third' component={Third}/>
-        </section>
-        <Drawer open={open} onClose={toggleDrawer}>
-          <div 
-            style={{width:250}}
-            role='presentation'
-            onClick={toggleDrawer}
-            onKeyDown={toggleDrawer}
-          >
-            <List>
-              {links.map((link)=>(
-                <ListItem button key={link.url} component={link} to={link.url}>
-                  <Switch>
-                    <Route 
-                      exact
-                      path={link.url}
-                      render={()=>(
-                        <ListItemText 
-                          primary={link.name}
-                          primaryTypographyProps={{color: "primary"}}
-                        />
-                      )}
-                    />
-                    <Route 
-                      path='/'
-                      render={()=><ListItemText primary={link.name}/>}
-                    />
-                  </Switch>
-                </ListItem>
-              ))}
-            </List>
-          </div>
-        </Drawer>
-      </Router>
-    </div>
-  )
+    <Router>
+      <Button onClick={toggleDrawer}>Open Nav</Button>
+      <section>
+        <Route path="/first" element={<First />} />
+        <Route path="/second" element={<Second />} />
+        <Route path="/third" element={<Third />} />
+      </section>
+      <Drawer open={open} onClose={toggleDrawer}>
+        <div
+          style={{ width: 250 }}
+          role="presentation"
+          onClick={toggleDrawer}
+          onKeyDown={toggleDrawer}
+        >
+          <List>
+            {links.map((link) => (
+              <ListItem button key={link.url} component={Link} to={link.url}>
+                <Switch>
+                  <Route
+                    exact
+                    path={link.url}
+                    render={() => (
+                      <ListItemText
+                        primary={link.name}
+                        primaryTypographyProps={{ color: "primary" }}
+                      />
+                    )}
+                  />
+                  <Route
+                    path="/"
+                    render={() => <ListItemText primary={link.name} />}
+                  />
+                </Switch>
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Drawer>
+    </Router>
+  );
 }
 
 Exercise3.defaultProps = {
-  links:[
-    {url:"/first" , name:"First Page"},
+  links: [
+    { url: "/first", name: "First Page" },
     { url: "/second", name: "Second Page" },
-    { url: "/third", name: "Third Page" }
+    { url: "/third", name: "Third Page" },
   ]
-}
+};
 
-
+export default Exercise3;
